b8f47e974dc41bc2da032a320c9125ecd634dd8d
shetharpan14 <shetharpan14@gmail.com>
2025-05-18 13:53:13 +0530

    Package Updates

--- SalesRepresentative.Tests/obj/Debug/net8.0/SalesRepresentative.Tests.AssemblyInfo.cs
+++ SalesRepresentative.Tests/obj/Debug/net8.0/SalesRepresentative.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesRepresentative.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050523f3246dd88c274582a0720ff2399c4d1a36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab083949a277b47aff7eb6c80627840f2635fe32")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesRepresentative.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesRepresentative.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
